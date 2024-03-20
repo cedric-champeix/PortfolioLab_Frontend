71362adb89785b8e34447f03a0adb864881927b3
piryth <arnaud.endignous@etu.univ-grenoble-alpes.fr>
2024-03-20 13:06:06 +0000

    Removed unused Yup configuration

--- frontend/src/config/yup/schema.js
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup"
-
-export const registerSchema = yup.object().shape({
-    firstName: yup.string().required(),
-    lastName: yup.string().required(),
-    email: yup.string().email().required(),
-    password: yup.string().min(8).max(32).required(),
-    confirmPassword: yup.string().oneOf([yup.ref("password"),null]).required()
-});

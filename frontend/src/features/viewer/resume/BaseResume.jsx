e1b2e8223324285022286e51269302ded7c407f0
Cédric Champeix <cedric.rchampeix@gmail.com>
2024-04-25 17:21:05 +0100

    Used print instead of to pdf to create a pdf of the component

--- frontend/src/features/viewer/resume/BaseResume.jsx
+++ frontend/src/features/viewer/resume/BaseResume.jsx
@@ -1,3 +1,4 @@
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
@@ -7,8 +8,6 @@ import Avatar from "@mui/material/Avatar";
 import ResumeContacts from "./components/ResumeContacts.jsx";
 import IconButton from "@mui/material/IconButton";
 import DownloadForOfflineIcon from "@mui/icons-material/DownloadForOffline.js";
-import React, {useEffect, useState} from "react";
-import {Margin, Resolution, usePDF} from "react-to-pdf";
 import profilePlaceHolder from "../../../assets/icons/blank-profile-picture.png";
 import {constants} from "../../../constants.js";
 import ResumeProfile from "./components/ResumeProfile.jsx";
@@ -17,11 +16,10 @@ import ResumeFormation from "./components/ResumeFormation.jsx";
 import ResumeSkills from "./components/ResumeSkills.jsx";
 import ResumeLanguages from "./components/ResumeLanguages.jsx";
 import ResumeHobbies from "./components/ResumeHobbies.jsx";
+import { useReactToPrint } from 'react-to-print';
 
 
 export default function BaseResume({userResume}) {
-
-    console.log("RESUME: ", userResume);
 
     const [image, setImage] = useState(profilePlaceHolder);
 
@@ -31,7 +29,6 @@ export default function BaseResume({userResume}) {
 
     useEffect(() => {
         setImage(userResume.resume.Image ? constants.BACKEND_URL + userResume.resume.Image.path : profilePlaceHolder)
-        console.log("IMAGE: ", image)
     }, [userResume]);
 
     function capitalize(str) {
@@ -41,35 +38,24 @@ export default function BaseResume({userResume}) {
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
 
-    const pdfOptions = {
-        // default is Resolution.MEDIUM = 3, which should be enough, higher values
-        // increases the image quality but also the size of the PDF, so be careful
-        // using values higher than 10 when having multiple pages generated, it
-        // might cause the page to crash or hang.
-        resolution: Resolution.MEDIUM,
-        filename: `${userResume.firstName}_${userResume.lastName}_CV.pdf`,
-        page: {
-            // margin is in MM, default is Margin.NONE = 0
-            margin: Margin.SMALL,
-            // default is 'A4'
-            format: 'A4'
-        }
-    };
+    const myRef = React.createRef();
 
-    const {toPDF, targetRef} = usePDF(pdfOptions);
+    const handlePrint = useReactToPrint({
+        content: () => myRef.current,
+        documentTitle: userResume.firstName + "_" + userResume.lastName + "_CV"
+    });
 
     return <React.Fragment>
         <Paper elevation={12}
                sx={{
-                   padding: '5mm',
                    width: '210mm', // A4 width
-                   height: '297mm', // A4 height
                    margin: '4vh auto'
                }}>
 
             <Box component="div"
-                 ref={targetRef}
-                 sx={{// Adjust padding as needed
+                 ref={myRef}
+                 sx={{
+                     padding: '5mm',
                      display: 'flex',
                      flexDirection: 'column',
                      alignItems: 'center'
@@ -118,7 +104,7 @@ export default function BaseResume({userResume}) {
         <IconButton variant="contained"
                     color="primary"
                     sx={{position: "fixed", bottom: "4%", right: "4%"}}
-                    onClick={() => toPDF()}>
+                    onClick={() => handlePrint()}>
             <DownloadForOfflineIcon sx={{fontSize: "70px"}}/>
         </IconButton>
     </React.Fragment>

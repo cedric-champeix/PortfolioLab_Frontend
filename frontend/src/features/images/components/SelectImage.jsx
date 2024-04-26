826a873ba1d30d70e59f97b4f7519573dc3bb015
Cédric Champeix <cedric.rchampeix@gmail.com>
2024-04-26 16:26:02 +0100

    Added the possibility to unselect images and skills

--- frontend/src/features/images/components/SelectImage.jsx
+++ frontend/src/features/images/components/SelectImage.jsx
@@ -1,27 +1,33 @@
+import React, {useState} from "react";
 import Grid from "@mui/material/Grid";
 import ImageCard from "./ImageCard.jsx";
 import {DialogActions} from "@mui/material";
 import Button from "@mui/material/Button";
-import {useState} from "react";
 import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
 
 
 export default function SelectImage({images, update, remove, toggle, callback}) {
 
-    const [selectedImage, setSelectedImage] = useState()
+    const [selectedImage, setSelectedImage] = useState(null)
 
     const handleSubmit = () => {
         callback(selectedImage)
         toggle()
     }
 
-    return <div>
+    return <React.Fragment>
+        {images.length === 0 && <Typography margin="25px">You did not upload any image yet.</Typography>}
         <Grid container style={{height: "100%", maxHeight: "75vh", overflow: "auto"}}  sx={{p: 3}} spacing={2}>
             {images.map((image) =>
                 <Grid item
                       key={image.id}
                       onClick={() => {
-                          setSelectedImage(image)
+                          if (selectedImage && selectedImage.id === image.id) {
+                              setSelectedImage(null)
+                          } else {
+                              setSelectedImage(image)
+                          }
                       }}>
                     <ImageCard image={image}
                                update={update}
@@ -34,7 +40,7 @@ export default function SelectImage({images, update, remove, toggle, callback}) 
             <Button onClick={toggle} color="error">Close</Button>
             <Button disabled={!selectedImage} onClick={handleSubmit}>Select</Button>
         </DialogActions>
-    </div>
+    </React.Fragment>
 
 }
 

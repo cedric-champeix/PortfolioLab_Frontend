9cd906aaef943344bae6e41e8a0016b3517977b0
Cédric Champeix <cedric.rchampeix@gmail.com>
2024-04-28 01:09:27 +0100

    Resolved problem with languages going out of the screen

--- frontend/src/features/viewer/resume/components/ResumeLanguages.jsx
+++ frontend/src/features/viewer/resume/components/ResumeLanguages.jsx
@@ -3,7 +3,7 @@ import Divider from "@mui/material/Divider";
 import Box from "@mui/material/Box";
 import React from "react";
 import PropTypes from "prop-types";
-import {Chip, Stack} from "@mui/material";
+import {Chip} from "@mui/material";
 
 
 export default function ResumeLanguages({languages}) {
@@ -11,15 +11,14 @@ export default function ResumeLanguages({languages}) {
     return <Box width="100%" display={languages.length !== 0 ? "block" : "none"}>
         <Typography variant="h4" color="primary">Languages</Typography>
         <Divider/>
-        <Stack sx={{padding: '3mm'}} direction="row" spacing={1}>
+        <Box sx={{padding: '3mm', display: 'flex', gap: 1, flexWrap: 'wrap'}}>
             {languages.map((language) => (
-
                 <Chip key={language.id} label={
                     <Typography variant="body1" sx={{fontWeight: "600"}}>
                         {language.name}: {language.level}
                     </Typography>}/>
             ))}
-        </Stack>
+        </Box>
     </Box>
 }
 ResumeLanguages.propTypes = {
